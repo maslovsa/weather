869b74e4570c812647540ca006c9f712c56f134d
Maslov Sergey <sergey.maslov@rokolabs.com>
2015-08-10 09:15:10 +0300

    init

---
+++ weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weather
+//
+//  Created by Maslov Sergey on 10.08.15.
+//  Copyright (c) 2015 ROKOLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
