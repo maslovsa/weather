fba6033051104546881b7f0e5afc26488cd60e24
Maslov Sergey <sergey.maslov@rokolabs.com>
2015-08-10 10:03:09 +0300

    DONE location

--- weather/ViewController.m
+++ weather/ViewController.m
@@ -7,21 +7,82 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>{
+	CLLocationManager *locationManager;
+	CLLocation *currentLocation;
+}
+@property (weak, nonatomic) IBOutlet UILabel *latitude;
+@property (weak, nonatomic) IBOutlet UILabel *longitude;
+@property (weak, nonatomic) IBOutlet UILabel *place;
 
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+	[super viewDidLoad];
+	[self CurrentLocationIdentifier];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)CurrentLocationIdentifier {
+	// ---- For getting current gps location
+	locationManager = [CLLocationManager new];
+	locationManager.delegate = self;
+	locationManager.distanceFilter = kCLDistanceFilterNone;
+	locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+	[locationManager startUpdatingLocation];
+	// ------
+}
+
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+	NSLog(@"didFailWithError: %@", error);
+	UIAlertView *errorAlert = [[UIAlertView alloc]
+	                           initWithTitle:@"Error" message:@"Failed to Get Your Location" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+	[errorAlert show];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+
+	currentLocation = [locations objectAtIndex:0];
+	[locationManager stopUpdatingLocation];
+	CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+	[geocoder reverseGeocodeLocation:currentLocation completionHandler:^(NSArray *placemarks, NSError *error)
+	{
+	    if (!(error)) {
+	        CLPlacemark *placemark = [placemarks objectAtIndex:0];
+	        
+	        NSString *Area = [[NSString alloc] initWithString:placemark.locality];
+	        NSString *Country = [[NSString alloc] initWithString:placemark.country];
+	        NSString *CountryArea = [NSString stringWithFormat:@"%@, %@", Area, Country];
+	        _place.text = CountryArea;
+		} else {
+	        NSLog(@"Geocode failed with error %@", error);
+	        NSLog(@"\nCurrent Location Not Detected\n");
+		}
+	}];
+	
+}
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+	switch (status) {
+	case kCLAuthorizationStatusNotDetermined: {
+		NSLog(@"User still thinking..");
+	} break;
+	case kCLAuthorizationStatusDenied: {
+		NSLog(@"User hates you");
+	} break;
+	case kCLAuthorizationStatusAuthorizedWhenInUse:
+	case kCLAuthorizationStatusAuthorizedAlways: {
+		[locationManager startUpdatingLocation];
+	} break;
+	default:
+		break;
+	}
 }
 
 @end
